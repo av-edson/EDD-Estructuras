67bccef6332fbc81935f75cd317e7ab26ab572dc
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2020-12-04 19:19:37 -0600

    Agregar nodo raiz al arbol

--- estructuras/BsTree.py
+++ estructuras/BsTree.py
@@ -5,8 +5,22 @@ class Node:
         self.derecho = None
         self.izquierdo = None
         self.altura = None
-       
+
+    """ Regresa un String para graficar el Arbol"""
+    def getDato(self):
+        return str(self.dato)
+
 
 class ArbolB:
     def __init__(self):
         self.raiz = None
+
+    def agregar(self, valor):
+        if self.raiz is None:
+            self.raiz = self.nuevoNodo(valor)
+        else:
+            print('agregar')
+
+    def nuevoNodo(self, valor):
+        return Node(valor)
+
