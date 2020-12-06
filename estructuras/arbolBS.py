4fbd6e46b31921457d1d0cb489067a7849d9f50e
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2020-12-06 14:14:41 -0600

    Eliminar Terminado

--- estructuras/arbolBS.py
+++ estructuras/arbolBS.py
@@ -1,5 +1,4 @@
 import graficadorArboles as Gr
-import random
 
 
 # CLASE PARA EL NODO
@@ -78,21 +77,59 @@ class ArbolBS:
                 else:
                     padre.izquierdo = None
                 return True
+            # tiene un hijo derecho
+            elif arbol.derecho is not None and arbol.izquierdo is None:
+                print('hijo derecho')
+                if padre.derecho == arbol:
+                    padre.derecho = arbol.derecho
+                else:
+                    padre.izquierdo = arbol.derecho
+            # tiene un hijo izquierdo
+            elif arbol.izquierdo is not None and arbol.derecho is None:
+                if padre.izquierdo == arbol:
+                    padre.izquierdo = arbol.izquierdo
+                else:
+                    padre.derecho = arbol.izquierdo
+            # tiene dos hijos
+            else:
+                # si es raiz
+                if padre is None:
+                    aux = self.getNodoEliminar(arbol.derecho, arbol)
+                    arbol.dato = aux
+                elif padre.izquierdo == arbol:
+                    # solo hay que remplazar el valor
+                    aux = self.getNodoEliminar(arbol.derecho, arbol)
+                    padre.izquierdo.dato = aux
+                else:
+                    padre.derecho = self.getNodoEliminar(arbol.derecho, arbol)
+            return True
+
+    def getNodoEliminar(self, nodo, padre):
+        if nodo.izquierdo is None:
+            padre.izquierdo = None
+            return nodo.getDato()
+        else:
+            return self.getNodoEliminar(nodo.izquierdo, nodo)
 
 
 # graficador e ingreso terminado
 arbol = ArbolBS()
-arbol.agregar(50)
-arbol.agregar(52)
-arbol.agregar(48)
 arbol.agregar(55)
-arbol.agregar(43)
+arbol.agregar(57)
+arbol.agregar(50)
+arbol.agregar(60)
+arbol.agregar(48)
+arbol.agregar(56)
+arbol.agregar(53)
+arbol.agregar(51)
+arbol.agregar(49)
+arbol.agregar(40)
+arbol.agregar(54)
 
 # para graficar
 graficador = Gr.Graficador(arbol, 'bs')
 graficador.exportar()
 
-arbol.eliminarElemento(43, arbol.raiz, None)
 arbol.eliminarElemento(55, arbol.raiz, None)
 
 graficador = Gr.Graficador(arbol, 'bs')
