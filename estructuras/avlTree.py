c89a3740b5a3b0e24359ea7ede50c8b9cd914ab8
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2020-12-06 19:12:25 -0600

    peso y graficacion de avl

--- estructuras/avlTree.py
+++ estructuras/avlTree.py
@@ -1,3 +1,8 @@
+import random as rd
+import graficadorArboles as graf
+
+
+
 class Nodo:
     def __init__(self, valor):
         self.dato = valor
@@ -18,3 +23,44 @@ class AvlTree:
     def agregar(self, valor):
         if self.raiz is None:
             self.raiz = Nodo(valor)
+        else:
+            self.raiz = self.__recursiveAdd(valor, self.raiz)
+
+    def __recursiveAdd(self, valor, arbol):
+        if valor < arbol.dato:
+            if arbol.izquierdo is None:
+                arbol.izquierdo = Nodo(valor)
+            else:
+                arbol.izquierdo = self.__recursiveAdd(valor, arbol.izquierdo)
+        elif valor == arbol.dato:
+            print('valor repetido')
+        else:
+            if arbol.derecho is None:
+                arbol.derecho = Nodo(valor)
+            else:
+                arbol.derecho = self.__recursiveAdd(valor, arbol.derecho)
+        arbol.peso = self.__pesoMax(arbol) + 1
+        return arbol
+
+    def __getPesoNodo(self, nodo):
+        if nodo is None:
+            return -1
+        else:
+            return int(nodo.peso)
+
+    def __pesoMax(self, nodo):
+        if self.__getPesoNodo(nodo.derecho) > self.__getPesoNodo(nodo.izquierdo):
+            return self.__getPesoNodo(nodo.derecho)
+        else:
+            return self.__getPesoNodo(nodo.izquierdo)
+
+
+
+
+g = AvlTree();
+g.agregar(50)
+for i in range(15):
+    g.agregar(rd.randint(0, 100))
+graficador  = graf.Graficador(g, 'avl')
+graficador.exportar()
+
